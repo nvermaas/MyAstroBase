ee6167f6fe09125dc98ea225b603592689758ba3
Nico Vermaas <vermaas@astron.nl>
2020-07-12 17:32:52 +0200

    switched backend from raspberry pi to mintbox

--- astrobase_services_pip/astrobase_scripts/astrobase_start_services.sh
+++ astrobase_services_pip/astrobase_scripts/astrobase_start_services.sh
@@ -3,13 +3,13 @@
 # after pip install, start as production: nohup astrobase_start_services.sh &
 
 # ingest looks for new images in the 'landing_pad' folder and moves them to 'raw' observations
-astrobase_service -o ingest --interval 30 --astrobase_host http://raspiastro:8008/astrobase --local_landing_pad ~/www/astrobase/landing_pad --local_data_dir ~/www/astrobase/data -v &
+astrobase_service -o ingest --interval 30 --astrobase_host http://uilennest.net/my_astrobase --local_landing_pad ~/www/astrobase/landing_pad --local_data_dir ~/www/astrobase/data -v &
 
 # submits a new job and checks its progress
-astrobase_service -o submit --interval 30 --astrobase_host http://raspiastro:8008/astrobase --local_landing_pad ~/www/astrobase/landing_pad --local_data_dir ~/www/astrobase/data --local_data_url http://uilennest.net/astrobase/data -v &
+astrobase_service -o submit --interval 30 --astrobase_host http://uilennest.net/my_astrobase --local_landing_pad ~/www/astrobase/landing_pad --local_data_dir ~/www/astrobase/data --local_data_url http://uilennest.net/astrobase/data -v &
 
 # processor submits a new job and handles the results
-astrobase_service -o processor --interval 30 --astrobase_host http://raspiastro:8008/astrobase --local_landing_pad ~/www/astrobase/landing_pad --local_data_dir ~/www/astrobase/data --local_data_url http://uilennest.net/astrobase/data -v &
+astrobase_service -o processor --interval 30 --astrobase_host http://uilennest.net/my_astrobase --local_landing_pad ~/www/astrobase/landing_pad --local_data_dir ~/www/astrobase/data --local_data_url http://uilennest.net/astrobase/data -v &
 
 # remove data and database entries
-astrobase_service -o cleanup --interval 60 --astrobase_host http://raspiastro:8008/astrobase --local_landing_pad ~/www/astrobase/landing_pad --local_data_dir ~/www/astrobase/data -v &
+astrobase_service -o cleanup --interval 60 --astrobase_host http://uilennest.net/my_astrobase --local_landing_pad ~/www/astrobase/landing_pad --local_data_dir ~/www/astrobase/data -v &
