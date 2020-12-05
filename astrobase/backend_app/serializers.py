ab5c08dbddb3e12520cb2270ecadefe66025cdf7
Nico Vermaas <vermaas@astron.nl>
2020-12-05 07:32:53 +0100

    making collection serializer leaner

--- astrobase/backend_app/serializers.py
+++ astrobase/backend_app/serializers.py
@@ -76,13 +76,84 @@ class ObservationSerializer(serializers.ModelSerializer):
                   'image_type','children')
 
 
+
+class ObservationFullSerializer(serializers.ModelSerializer):
+    # this adds a 'generated_dataproducts' list with hyperlinks to the Observation API.
+    # note that 'generated_dataproducts' is not defined in the DataProduct model,
+    # but comes from the related_field in Observation.parent.
+
+    generated_dataproducts = serializers.StringRelatedField(
+        many=True,
+        required=False,
+    )
+
+    status_history = serializers.StringRelatedField(
+        many=True,
+        required=False,
+    )
+
+    parent = serializers.PrimaryKeyRelatedField(
+        many=False,
+        queryset=Observation.objects.filter(task_type='master'),
+        required=False,
+        allow_null=True,
+    )
+
+    children = serializers.StringRelatedField(
+        many=True,
+        required=False,
+        read_only=True
+    )
+
+    class Meta:
+        model = Observation
+        fields = ('id','task_type', 'name', 'instrument','filter','process_type','taskID',
+                  'field_name','field_ra','field_dec','field_fov',
+                  'ra_min', 'ra_max','dec_min', 'dec_max','ra_dec_fov','date','size',
+                  'derived_raw_image','derived_sky_plot_image','derived_annotated_image',
+                  'derived_annotated_grid_image','derived_annotated_stars_image','derived_sky_globe_image',
+                  'derived_fits',
+                  'my_status','new_status','status_history','job','astrometry_url','url',
+                  'generated_dataproducts','data_location', 'quality','description',
+                  'parent','derived_parent_taskid',
+                  'exposure_in_seconds','iso','focal_length','stacked_images','magnitude',
+                  'image_type','children')
+
+
+class ObservationCollectionSerializer(serializers.ModelSerializer):
+    # this adds a 'generated_dataproducts' list with hyperlinks to the Observation API.
+    # note that 'generated_dataproducts' is not defined in the DataProduct model,
+    # but comes from the related_field in Observation.parent.
+
+    parent = serializers.PrimaryKeyRelatedField(
+        many=False,
+        queryset=Observation.objects.filter(task_type='master'),
+        required=False,
+        allow_null=True,
+    )
+
+    class Meta:
+        model = Observation
+        fields = ('id','name', 'instrument','filter','taskID',
+                  'field_name','field_ra','field_dec','field_fov',
+                  'ra_min', 'ra_max','dec_min', 'dec_max','ra_dec_fov','date','size',
+                  'derived_raw_image','derived_sky_plot_image','derived_annotated_image',
+                  'derived_annotated_grid_image','derived_annotated_stars_image','derived_sky_globe_image',
+                  'derived_fits',
+                  'my_status','new_status','job','astrometry_url','url',
+                  'quality','description',
+                  'parent','derived_parent_taskid',
+                  'exposure_in_seconds','iso','focal_length','stacked_images','magnitude',
+                  'image_type')
+
+
 class CollectionSerializer(serializers.ModelSerializer):
-    observations = ObservationSerializer(many=True, read_only=True)
+    observations = ObservationCollectionSerializer(many=True, read_only=True)
 
     class Meta:
         model = Collection
-        #fields = ('id','date','name','collection_type','description','observations')
-        fields = "__all__"
+        fields = ('id','date','name','collection_type','description','observations')
+        # fields = "__all__"
         # this expands the observations to more than just the id.
         #depth=1
 
