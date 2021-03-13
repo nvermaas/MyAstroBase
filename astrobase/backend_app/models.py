684846a6a6ad92729ac60457f7074bb7616f6e09
Nico Vermaas <vermaas@astron.nl>
2021-03-13 10:21:25 +0100

    pushing a build to test actions

--- astrobase/backend_app/models.py
+++ astrobase/backend_app/models.py
@@ -159,7 +159,7 @@ class Observation(TaskObject):
     # magnitude = models.FloatField(null = True, blank=True)
     magnitude = models.CharField(max_length=5, null=True, blank=True)
     image_type = models.CharField(max_length=20, null=True, choices = IMAGE_TYPE_CHOICES, default="other")
-    used_in_hips = models.BooleanField(default=False)
+    used_in_hips = models.BooleanField(default=True)
 
     # relationships
     parent = models.ForeignKey('self', related_name='children', on_delete=models.SET_NULL, null=True, blank=True)
