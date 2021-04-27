3a7c25e8f9dc86ee067831c29e8c7bdf0ed46113
Nico Vermaas <vermaas@astron.nl>
2021-04-27 14:04:40 +0200

    test if numpy, skyfield and panda's can be installed

--- astrobase/transients_app/services/algorithms.py
+++ astrobase/transients_app/services/algorithms.py
@@ -1,6 +1,8 @@
 import requests
 # from astroquery.mpc import MPC
-
+import skyfield
+from skyfield.api import load
+from skyfield.data import mpc
 # fetch properties of the orbits of a minor planet by name
 def get_minor_planets_webservice(name):
     url = 'https://minorplanetcenter.net/web_service/search_orbits'
@@ -22,7 +24,22 @@ def get_minor_planets_webservice(name):
 
 # fetch properties of the orbits of a minor planet by name
 def get_transients(name):
+    # https://rhodesmill.org/skyfield/example-plots.html#drawing-a-finder-chart-for-comet-neowise
     # https://astroquery.readthedocs.io/en/latest/mpc/mpc.html
+    ts = load.timescale()
+    t = ts.utc(2020, 5, 13, 10, 32)
+    eph = load('de421.bsp')
+    astrometric = eph['earth'].at(t).observe(eph['psyche'])
+    ra, dec, distance = astrometric.radec()
+    print(ra, dec, sep='\n')
+
+    with load.open(mpc.COMET_URL) as f:
+        comets = mpc.load_comets_dataframe(f)
+
+    comets = (comets.sort_values('reference')
+              .groupby('designation', as_index=False).last()
+              .set_index('designation', drop=False))
+
 
     #result = MPC.query_object('asteroid', name=name)
     #eph = MPC.get_ephemeris(name,start='2021-01-01',step='1h')
