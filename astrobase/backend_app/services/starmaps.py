6d558efdb80b962d55ef5c934fdce454b50630dd
Nico Vermaas <vermaas@astron.nl>
2021-05-23 14:33:48 +0200

    switch to python 3.9.5-slim

--- astrobase/backend_app/services/starmaps.py
+++ astrobase/backend_app/services/starmaps.py
@@ -10,4 +10,81 @@ from skyfield.projections import build_stereographic_projection
 # /astrobase/post_dataproducts?taskid=190405001
 def create_starmap(command, observation_id):
     url = ''
+
+    # The comet is plotted on several dates `t_comet`.  But the stars only
+    # need to be drawn once, so we take the middle comet date as the single
+    # time `t` we use for everything else.
+
+    ts = load.timescale()
+    t_comet = ts.utc(2020, 7, range(17, 27))
+    t = t_comet[len(t_comet) // 2]  # middle date
+
+    # An ephemeris from the JPL provides Sun and Earth positions.
+
+    eph = load('de421.bsp')
+    sun = eph['sun']
+    earth = eph['earth']
+
+    # The Minor Planet Center data file provides the comet orbit.
+
+    with load.open(mpc.COMET_URL) as f:
+        comets = mpc.load_comets_dataframe(f)
+
+    comets = (comets.sort_values('reference')
+              .groupby('designation', as_index=False).last()
+              .set_index('designation', drop=False))
+
+    row = comets.loc['C/2020 F3 (NEOWISE)']
+    comet = sun + mpc.comet_orbit(row, ts, GM_SUN)
+
+    # The Hipparcos mission provides our star catalog.
+
+    with load.open(hipparcos.URL) as f:
+        stars = hipparcos.load_dataframe(f)
+
+    # And the constellation outlines come from Stellarium.  We make a list
+    # of the stars at which each edge stars, and the star at which each edge
+    # ends.
+
+    url = ('https://raw.githubusercontent.com/Stellarium/stellarium/master'
+           '/skycultures/western_SnT/constellationship.fab')
+
+    with load.open(url) as f:
+        constellations = stellarium.parse_constellations(f)
+
+    edges = [edge for name, edges in constellations for edge in edges]
+    edges_star1 = [star1 for star1, star2 in edges]
+    edges_star2 = [star2 for star1, star2 in edges]
+
+    # We will center the chart on the comet's middle position.
+
+    center = earth.at(t).observe(comet)
+    projection = build_stereographic_projection(center)
+    field_of_view_degrees = 45.0
+    limiting_magnitude = 7.0
+
+    # Now that we have constructed our projection, compute the x and y
+    # coordinates that each star and the comet will have on the plot.
+
+    star_positions = earth.at(t).observe(Star.from_dataframe(stars))
+    stars['x'], stars['y'] = projection(star_positions)
+
+    comet_x, comet_y = projection(earth.at(t_comet).observe(comet))
+
+    # Create a True/False mask marking the stars bright enough to be
+    # included in our plot.  And go ahead and compute how large their
+    # markers will be on the plot.
+
+    bright_stars = (stars.magnitude <= limiting_magnitude)
+    magnitude = stars['magnitude'][bright_stars]
+    marker_size = (0.5 + limiting_magnitude - magnitude) ** 2.0
+
+    # The constellation lines will each begin at the x,y of one star and end
+    # at the x,y of another.  We have to "rollaxis" the resulting coordinate
+    # array into the shape that matplotlib expects.
+
+    xy1 = stars[['x', 'y']].loc[edges_star1].values
+    xy2 = stars[['x', 'y']].loc[edges_star2].values
+    lines_xy = np.rollaxis(np.array([xy1, xy2]), 1)
+
     return url
