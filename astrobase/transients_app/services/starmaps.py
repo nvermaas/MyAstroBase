518758a468ac3c6eeedb82eab6aa4602fb73b298
Nico Vermaas <vermaas@astron.nl>
2021-10-24 16:12:08 +0200

    get matplotlib out of the way in production

--- astrobase/transients_app/services/starmaps.py
+++ astrobase/transients_app/services/starmaps.py
@@ -1,8 +1,11 @@
 # create starmaps using skyfield and matplotlib
 
 import numpy as np
-from matplotlib import pyplot as plt
-from matplotlib.collections import LineCollection
+try:
+    from matplotlib import pyplot as plt
+    from matplotlib.collections import LineCollection
+except:
+    pass
 
 from skyfield.api import Star, load
 from skyfield.constants import GM_SUN_Pitjeva_2005_km3_s2 as GM_SUN
