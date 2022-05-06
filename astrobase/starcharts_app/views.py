2cb32000bae3259e42c3ede430ef77421eb21f77
Nico Vermaas <vermaas@astron.nl>
2022-05-06 20:57:23 +0200

    creating form

--- astrobase/starcharts_app/views.py
+++ astrobase/starcharts_app/views.py
@@ -61,17 +61,22 @@ def CreateStarChart(request):
 #http://localhost:8000/my_astrobase/create-starchart?ra_min=44&ra_max=56&dec_min=10.75&dec_max=19.15&mag=10
 def FormStarChart(request, name=None):
 
+    starchart = StarChart.objects.get(name=name)
+
     # a POST means that the form is filled in and should be stored in the database
     if request.method == "POST":
 
-        form = StarChartForm(request.POST)
-        name = request.GET.get('name', 'my_starchart')
+        form = StarChartForm(request.POST, instance=starchart)
+
+        name = starchart.name
+        mag = starchart.magnitude_limit
         ra_min = float(request.GET.get('ra_min','44'))/15
         ra_max = float(request.GET.get('ra_max','56'))/15
         dec_min = float(request.GET.get('dec_min','10.75'))
         dec_max = float(request.GET.get('dec_max','19.25'))
         mag = float(request.GET.get('mag','10'))
 
+        # todo: just give the (alterned) starchart als parameter instead of this whole list?
         starchart, starchart_url_media = create_starchart(name,ra_min,ra_max,dec_min,dec_max,mag)
 
         return render(request, "starcharts_app/do-starchart.html", {'starchart':starchart,'starchart_url_media': starchart_url_media})
