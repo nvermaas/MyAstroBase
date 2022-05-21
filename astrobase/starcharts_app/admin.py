a688b57e1e9185b951d48cf5caff5eb28a5c4226
Nico Vermaas <vermaas@astron.nl>
2022-05-21 14:58:36 +0200

    complete stars model

--- astrobase/starcharts_app/admin.py
+++ astrobase/starcharts_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import StarChart, Scheme
+from .models import StarChart, Scheme, Stars
 
 # Register your models here.
+admin.site.register(Stars)
 admin.site.register(Scheme)
 admin.site.register(StarChart)
