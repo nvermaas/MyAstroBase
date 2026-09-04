8f17debbcf5c92019e2f82588ad2c95f10ef6e2d
Vermaas <vermaas@astron.nl>
2026-09-04 20:43:15 +0200

    remove result backend (I'm not using it)

--- astrobase/backend_app/my_celery.py
+++ astrobase/backend_app/my_celery.py
@@ -8,7 +8,7 @@ except:
 
 print(f'connect to RABBITMQ_BROKER: {RABBITMQ_BROKER}')
 
-app = Celery('my_celery',backend='rpc://',broker=RABBITMQ_BROKER)
+app = Celery('my_celery',broker=RABBITMQ_BROKER)
 app.conf.task_ignore_result = True
 
 print(f'Celery conf: {app.conf} ')
